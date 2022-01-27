867ef835da288595a965fe84901067259d3fbd39
Aurelie Gedeon <aureliegedeon@Aurelies-Macbook-Pro.local>
2022-01-27 14:25:57 -0500

    added simple query

--- index.js
+++ index.js
@@ -25,13 +25,44 @@ initializeApp({
 const db = getFirestore();
 
 //create a collection called "restaurants"
+const restRef = db.collection("restaurants");
 
 //add each restaurant
-db.collection("restaurants")
-  .add(restaurants[1])
+restRef
+  .add(restaurants[3])
   .then((doc) => {
     console.log("Added restaurant", doc.id);
   })
   .catch((err) => {
     console.error(err);
   });
+
+//read one document
+restRef
+  .doc("WqrtRrrZikjTVlfw7Y3R")
+  .get()
+  .then((doc) => {
+    console.log(doc.id, " => ", doc.data());
+  })
+  .catch((err) => console.error(err));
+
+// get all documents
+restRef
+  .get()
+  .then((snapshot) => {
+    snapshot.forEach((doc) => {
+      console.log(doc.id, " => ", doc.data());
+    });
+  })
+  .catch(console.error);
+
+//find a document(s)/querying a collection
+restRef
+  .where("name", "==", "Bolay")
+  .get()
+  .then((snapshot) => {
+    snapshot.forEach((doc) => {
+      console.log(doc.data());
+    });
+  })
+  .catch(console.error);
