e1cbfb2e59c1ac3cfddb8c76622010d2a9bd6a49
Aurelie Gedeon <aureliegedeon@Aurelies-Macbook-Pro.local>
2022-01-27 12:07:03 -0500

    connected to firestore and added restaurants

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 //import our restaurants
 const restaurants = require("./restaurants.json");
 
-console.log(restaurants[0].name);
+//import a set of tools to talk to firebase and Firestore
+const {
+  initializeApp,
+  applicationDefault,
+  cert,
+} = require("firebase-admin/app");
+const {
+  getFirestore,
+  Timestamp,
+  FieldValue,
+} = require("firebase-admin/firestore");
+
+//import our credentials
+const credentials = require("./credentials.json");
+
+//connect to firebase services
+initializeApp({
+  credential: cert(credentials),
+});
 
 //connect to Firstore
+const db = getFirestore();
 
 //create a collection called "restaurants"
 
 //add each restaurant
+db.collection("restaurants")
+  .add(restaurants[1])
+  .then((doc) => {
+    console.log("Added restaurant", doc.id);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
